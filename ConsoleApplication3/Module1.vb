f30aacfca5dedf9afe1fbf93317bcba5599205a6
edes <edes@EDES-PC>
2016-10-16 14:56:33 +0200

    uservalue_and_message

---
+++ ConsoleApplication3/Module1.vb
@@ -0,0 +1,56 @@
+﻿Module Module1
+
+    Sub Main()
+        ''Console.WriteLine("type somthing and press Enter")
+        ''Dim userValue As String
+        ''userValue = Console.ReadLine()
+        ''Console.WriteLine("You typed:" & userValue)
+        ''Console.ReadLine()
+        'Dim message As String
+        'message = "type something and press Enter"
+        'Dim userValue As String
+        'message = "you typed something:" & userValue()
+        'Console.WriteLine("Would you prefer what is behind the door number 1,2,3?")
+        'Dim userValue As String = Console.ReadLine()
+        'Dim message As String = ""
+        'If userValue = "1" Then
+        '    message = "you won a new car!"
+        'ElseIf userValue = "2" Then
+        '    message = "you won a new boat!"
+        'ElseIf userValue = "3" Then
+        '    message = "you won a cat"
+        'Else
+        '    message = "sorry, you are a crazy boy, I do not understand your response"
+        'End If
+        'Console.WriteLine(message)
+        '        Console.ReadLine()
+        '    End Sub
+
+        'End Module
+
+        '        Console.WriteLine("Would you prefer what is behind the door number 1,2,3?")
+        '        Dim userValue As String = Console.ReadLine()
+        '        Dim message As String = IIf(userValue = "1", "car", "cat")
+        '        Console.WriteLine("you picked: {1} ... So won a {0}!", message, userValue)
+
+        '        Console.ReadLine()
+        '    End Sub
+
+        'End Module
+
+
+        'Dim x = 0
+        'Dim y = 0
+        'Dim a = 0
+        'Dim b = 0
+        'If x = y Then
+        '    stuff goes here
+
+        '    Conditional And operator 
+        'End If
+
+
+        Console.ReadLine()
+End Sub
+
+End Module
